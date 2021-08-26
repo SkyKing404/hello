3903f0b377e0a7585b00c4d7e5debbf4d5bf5a78
Joey Rager <jrager3@huskers.unl.edu>
2021-08-26 20:37:57 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * author: Joey rager
+ * date : 2020/08/26
+ * 
+ * Simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+int main(int argc, char **argv) {
+   
+    printf("Joey Rager!\n");
+    
+    return 0;
+}
